09004cbf1c37fdd9f38096e75c055ddb43cc0213
vonnue-assin <assin.anna@vonnue.com>
2025-08-05 12:26:14 +0530

    refactor:resolved the review comments

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,27 +1,33 @@
 import React, { ReactNode } from 'react';
 
+type ButtonVariant = 'primary' | 'secondary';
+
 import './styles.css';
 
-type ButtonProps = {
+interface ButtonProps {
   onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
-  type?: 'button' | 'submit';
+  type?: 'button' | 'submit' | 'reset';
   className?: string;
   disabled?: boolean;
+  variant?: ButtonVariant;
   children: ReactNode;
-};
+}
 
 const Button: React.FC<ButtonProps> = ({
   onClick,
   type = 'button',
   className = '',
   disabled = false,
+  variant = 'primary',
   children,
 }) => {
+  const buttonClasses = `common-button ${variant}-button ${className}`;
+
   return (
     <button
       onClick={onClick}
       type={type}
-      className={`common-button ${className}`}
+      className={buttonClasses}
       disabled={disabled}
     >
       {children}
