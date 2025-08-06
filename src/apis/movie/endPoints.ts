93b761548978ec410e6efde41498923434e025a2
vonnue-assin <assin.anna@vonnue.com>
2025-08-06 20:38:04 +0530

    feat:added new endpiont to render the nowplaying movie

--- src/apis/movie/endPoints.ts
+++ src/apis/movie/endPoints.ts
@@ -1,3 +1,5 @@
 export const endPoints = {
   getMovieGeneresList: () => `3/genre/movie/list?language=en`,
+  getNowPlayingMovies: () =>
+    `https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1`,
 };
