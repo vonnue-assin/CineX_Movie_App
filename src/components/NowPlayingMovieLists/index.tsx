d738f51c8488e77df626af84b6cd99f57d59a8af
vonnue-assin <assin.anna@vonnue.com>
2025-08-07 12:13:59 +0530

    refactor:updated the useQuery hook code

--- src/components/NowPlayingMovieLists/index.tsx
+++ src/components/NowPlayingMovieLists/index.tsx
@@ -6,11 +6,11 @@ export const NowPlayingMovieLists = () => {
   const { data: movies, isLoading, isError } = useGetNowPlayingMovies();
 
   if (isLoading) {
-    return <p>Loading Movies..</p>;
+    toast.success('Loading Movies..');
   }
 
   if (isError) {
-    return <p>Failed to load movies.Please try again</p>;
+    toast.error('Failed to load movies.Please try again');
   }
   if (!movies || movies.length === 0) return <p>No movies found.</p>;
 
