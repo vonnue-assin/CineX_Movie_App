e2daa3565c24687e7188f7787a590502ba24c086
vonnue-assin <assin.anna@vonnue.com>
2025-08-13 18:15:31 +0530

    refactor:resolved the review comments

--- src/components/MovieLists/index.tsx
+++ src/components/MovieLists/index.tsx
@@ -1,4 +1,5 @@
 import { toast } from 'react-toastify';
+import { ClipLoader } from 'react-spinners';
 
 import { useGetMovieList } from '../../apis/movie';
 import { MovieListCard } from '../MovieListCard';
@@ -9,20 +10,27 @@ export const MovieList = () => {
   const { data: movies, isLoading, isError } = useGetMovieList();
 
   if (isLoading) {
-    toast.success('Loading Movies..');
+    return (
+      <div className="loader-container">
+        <ClipLoader loading={isLoading} color="red" size={60} />
+      </div>
+    );
   }
 
   if (isError) {
-    toast.error('Failed to load movies.Please try again');
+    return <p>Failed to load movies.Please try again</p>;
   }
-  if (!movies || movies.length === 0) return <p>No movies found.</p>;
+  
+  if (movies?.length === 0) {
+    return <p>No movies found</p>;
+  }
 
   return (
     <>
       <h2 className="now-playing-movie-lists-title">Enjoy and Explore....</h2>
       <div className="now-playing-movie-container">
         <div className="now-playing-movie-list-container">
-          {movies.map(movie => (
+          {(movies ?? []).map(movie => (
             <MovieListCard key={movie.id} {...movie} />
           ))}
         </div>
