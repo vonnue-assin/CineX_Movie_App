5479c001c05d0e3fa0a3b0ec85df54256dd9e674
vonnue-assin <assin.anna@vonnue.com>
2025-08-14 16:55:44 +0530

    feat:Implemented a functionality to convert snakecases to camelCases

---
+++ src/components/ConvertKeysToCamelCase/index.tsx
@@ -0,0 +1,17 @@
+const snakeCaseToCamelCase = (s: string) => {
+  return s.replace(/([-_][a-z])/gi, $1 => {
+    return $1.toUpperCase().replace('-', '').replace('_', '');
+  });
+};
+
+export const convertKeysToCamelCase = (obj: any): any => {
+  if (Array.isArray(obj)) {
+    return obj.map(v => convertKeysToCamelCase(v));
+  } else if (obj !== null && typeof obj === 'object') {
+    return Object.keys(obj).reduce((acc: Record<string, any>, key) => {
+      acc[snakeCaseToCamelCase(key)] = convertKeysToCamelCase(obj[key]);
+      return acc;
+    }, {});
+  }
+  return obj;
+};
